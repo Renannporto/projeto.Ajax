e87d538ac08147bc625708df018d8bccafa30013
Renannporto <144285350+Renannporto@users.noreply.github.com>
2025-04-08 20:48:14 +0100

    atualizando o js

--- scripts.js
+++ scripts.js
@@ -7,15 +7,22 @@ document.addEventListener("DOMContentLoaded", function(){
     const linkElement = document.querySelector("#link");
 
     fetch("https://api.github.com/users/Renannporto")
-    .then(function(rest){
-        return rest.json();
-    })
-    .then(function(json){
-        usernameElement.innerText = json.login;
-        avatarElement.src = json.avatar_url;
-        followingElement.innerText = json.following;
-        followersElement.innerText = json.followers;
-        repos.innerText = json.public_repos;
-        linkElement.href = json.html_url;
-    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Erro na requisição: " + response.status);
+            }
+            return response.json();
+        })
+        .then(function (json) {
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            followersElement.innerText = json.followers;
+            repostElement.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+        })
+        .catch(function (error) {
+            console.error("Erro ao buscar dados do GitHub:", error);
+            usernameElement.innerText = "Erro ao carregar usuário";
+        });
 })
